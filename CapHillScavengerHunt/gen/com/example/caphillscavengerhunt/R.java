0da1d561dc01f57943ed4ea121384703dcce05a9
Chris Bubernak <chris.bubernak@gmail.com>
2013-06-28 09:54:43 -0700

    Moved Progress Bar & Text into its own subview
    -moved to its own view and positioned correctly in relative view
    -this portion of the view should remain static while the rest should
    change on a swipe

--- CapHillScavengerHunt/gen/com/example/caphillscavengerhunt/R.java
+++ CapHillScavengerHunt/gen/com/example/caphillscavengerhunt/R.java
@@ -24,13 +24,15 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080008;
+        public static final int ProgressLayout=0x7f080000;
+        public static final int action_settings=0x7f08000a;
         public static final int answerField=0x7f080004;
         public static final int back=0x7f080005;
-        public static final int clue=0x7f080001;
-        public static final int next=0x7f080002;
-        public static final int progressBar=0x7f080000;
-        public static final int progressText=0x7f080007;
+        public static final int clue=0x7f080008;
+        public static final int linearLayout1=0x7f080009;
+        public static final int next=0x7f080007;
+        public static final int progressBar=0x7f080001;
+        public static final int progressText=0x7f080002;
         public static final int question=0x7f080006;
         public static final int submit=0x7f080003;
     }
